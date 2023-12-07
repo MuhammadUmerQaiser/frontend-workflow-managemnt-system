44373202992b8f9fc1ab6d8e64fcaf8268bc4a26
MuhammadHARIS46 <muhammadhariskuk123@gmail.com>
2023-12-07 16:58:30 +0500

    file for otp and forgot password screen

---
+++ src/components/Auth/ForgotPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForgotPassword = () => {
+  return (
+    <div>
+      Screen if someone has forgot his password
+    </div>
+  );
+}
+
+export default ForgotPassword;
